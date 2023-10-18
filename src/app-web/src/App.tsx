e255a279788583824ed91cb90b966f4d69188e18
Rafael Penido <rafaelpenido1998@gmail.com>
2023-10-18 17:26:15 -0300

    ajustando tela login

--- src/app-web/src/App.tsx
+++ src/app-web/src/App.tsx
@@ -1,10 +1,10 @@
-import "./App.css";
-import Doctor from "./screens/doctor";
+import "./styles/Home.css";
+import Home from "./screens/home";
 
 function App() {
   return (
     <>
-      <Doctor />
+      <Home />
     </>
   );
 }
