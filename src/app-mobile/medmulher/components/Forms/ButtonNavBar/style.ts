bb49c76dc839301d485175fbfeec993c8831677c
Rafael Penido <rafaelpenido1998@gmail.com>
2023-11-10 17:02:57 -0300

    ajustando barra de navegação

--- src/app-mobile/medmulher/components/Forms/ButtonNavBar/style.ts
+++ src/app-mobile/medmulher/components/Forms/ButtonNavBar/style.ts
@@ -3,7 +3,7 @@ import styled from "styled-components/native";
 export const NavBar = styled.View`
     width: 100%;
     flex-direction: row; 
-    justify-content: space-between; 
+    justify-content: space-around; 
     padding: 8px;
     border-top-width: 2px;
     border-top-color: ${({theme}) => theme.colors.navbar};
