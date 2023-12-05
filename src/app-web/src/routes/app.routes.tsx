358625da4936ff9a4c1e84632de968f59d5438d1
Herbert Nordson <herbert_nordson@hotmail.com>
2023-12-05 11:06:22 -0300

    feat: ajustando rotas com autenticação

--- src/app-web/src/routes/app.routes.tsx
+++ src/app-web/src/routes/app.routes.tsx
@@ -10,22 +10,19 @@ import {
 import RegisterDoctor from "../screens/registerDoctor";
 
 export default function Routes() {
+  const auth = window.localStorage.getItem("token");
   return (
     <Switch>
-      {window.localStorage.getItem("token") ? (
-        <>
-          <Route path="/inicio" element={<Home />} />
-          <Route path="/cadastro-medico" element={<RegisterDoctor />} />
-          <Route path="/doutor" element={<Doctor />} />
-          <Route path="/perfil-medico" element={<Profile />} />
-        </>
-      ) : (
-        <>
-          <Route path="/redefinicao-de-senha" element={<RedefinePassword />} />
-          <Route path="/cadastro-paciente" element={<Register />} />
-          <Route path="/" element={<Login />} />
-        </>
-      )}
+      <>
+        <Route path="/inicio" element={!auth ? <Login /> : <Home />} />
+        <Route path="/cadastro-medico" element={!auth ? <Login /> : <RegisterDoctor />} />
+        <Route path="/doutor" element={!auth ? <Login /> : <Doctor />} />
+        <Route path="/perfil-medico" element={!auth ? <Login /> : <Profile />} />
+
+        <Route path="/redefinicao-de-senha" element={!auth ? <RedefinePassword /> : <Home />} />
+        <Route path="/cadastro-paciente" element={!auth ? <Register /> : <Home />} />
+        <Route path="/" element={!auth ? <Login /> : <Home />} />
+      </>
     </Switch>
   );
 }
